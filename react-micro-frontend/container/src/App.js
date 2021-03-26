76ccde48206cfecdcfa58fd6edbd2bd531567bf8
Mike Upton <me@uptonm.dev>
2021-03-25 22:19:31 -0400

    fix: fix conflicting classnames

--- react-micro-frontend/container/src/App.js
+++ react-micro-frontend/container/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
+import {BrowserRouter as Router} from "react-router-dom";
+
+import Header from "./components/Header";
 import {MarketingApp} from "./components/MarketingApp";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co"
+})
 
 export const App = () => {
     return (
-        <div>
-            <h1>Hello Container App!!!</h1>
-            <hr/>
-            <MarketingApp />
-        </div>
+        <Router>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </Router>
     )
 }
--- react-micro-frontend/marketing/src/App.js
+++ react-micro-frontend/marketing/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {StylesProvider} from "@material-ui/core/styles";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
 import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
 
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: "mkt"
+})
+
 export const App = () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <Router>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing} />
